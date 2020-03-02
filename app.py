b0f40e2e043cdcc5288fabde6c578642ac7912ea
digitalstuart <stuartrichardsdigital@gmail.com>
2020-03-02 11:32:14 +0000

    Emergency push, possible saving issues

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ if path.exists("env.py"):
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'car_share' 
@@ -41,14 +42,22 @@ def reply_to(lift_id):
 
 @app.route('/add_reply/<lift_id>', methods=['POST'])
 def add_reply(lift_id):
-    lifts = mongo.db.lifts
-    lifts.update( {'_id': ObjectId(lift_id)},
-    {   'offer_or_request':request.form.get('offer_or_request'),
-        'locations_start_name':request.form.get('locations_start_name'),
-        'locations_end_name': request.form.get('locations_end_name'),
-        'journey_details': request.form.get('journey_details'),
-        'date_of_travel': request.form.get('date_of_travel')
-    })   
+    # lifts = mongo.db.lifts
+    # lifts.update( {'_id': ObjectId(lift_id)},
+    # {   'offer_or_request':request.form.get('offer_or_request'),
+    #     'locations_start_name':request.form.get('locations_start_name'),
+    #     'locations_end_name': request.form.get('locations_end_name'),
+    #     'journey_details': request.form.get('journey_details'),
+    #     'date_of_travel': request.form.get('date_of_travel')        
+    # })
+    liftid = mongo.db.lifts.find_one({"_id": ObjectId(lift_id)})["_id"]
+    comments = mongo.db.comments
+    submit = {
+        'posted': datetime.now().strftime("%d/%m/%y"),
+        'text': request.form.get('text'),
+        'discussion_id': liftid
+    }
+    comments.insert_one(submit)
     return redirect(url_for('lifts')) 
 
 @app.route('/edit_lift/<lift_id>')
