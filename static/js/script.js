3e13d355004aa15499cc2248e80c538ccdb0d52a
digitalstuart <stuartrichardsdigital@gmail.com>
2020-03-07 15:47:49 +0000

    Final commit for .js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,7 @@
-//Get the button:
+// Function for 'Back to top' button
+
 mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
@@ -11,24 +9,12 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
 document.getElementById("myBtn").addEventListener ("click", function () {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
 
-/*
-document.getElementById("show-hide").addEventListener("click", function() {
-    let test = document.getElementsByClassName("reply");
-        if (test.style.display === "none") {
-            test.style.display = "block";
-        }
-        else {
-            test.style.display = "none";
-        }
-})
-*/
+// Function for show/hide toggle and arrow direction change on replies
 
 function replyFunction(childElement, id) {
   let replySection = document.getElementById("replies-for-" + id);
@@ -39,19 +25,10 @@ function replyFunction(childElement, id) {
   } else {
     replySection.style.display = 'none';
     childElement.innerHTML = '<strong>Show/hide replies <i class="fas fa-caret-down"></i></strong>';
-  }
-
-  /* let thisClick = document.getElementsByClassName('replies-show-hide')
-
-  if (!replySection.style.display || replySection.style.display == 'none') {
-    thisClick.innerHTML = '<strong>Show/hide replies <i class="fas fa-caret-up"></i></strong>';
-  } else {
-    thisClick.innerHTML = '<strong>Show/hide replies <i class="fas fa-caret-down"></i></strong>';
-  }
-
-  */
-
+  }  
 }
+
+// Function for show/hide toggle and arrow direction change on journey details
 
 function detailsFunction(childElement, id) {
   let detailsSection = document.getElementById("details-for-" + id);
